cafe731c8d28456fc368db52dfbb8b6167774996
mamat1123 <theeramat1123.ts@gmail.com>
2025-06-03 02:23:52 +0700

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:4173'], // Frontend URLs
+    origin: ['http://localhost:5173', 'http://localhost:4173', 'https://jtagco.vercel.app/'], // Frontend URLs
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
